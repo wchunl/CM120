e1ac1d6e1f0f59f979c24a8057f2b2b93ccfe43b
Shineng <stang38@ucsc.edu>
2019-06-07 21:52:15 -0700

    change art

--- js/prefabs/Child.js
+++ js/prefabs/Child.js
@@ -5,18 +5,19 @@ function Child(game, posx, posy) {
     
     // Set the character to face right
     this.anchor.setTo(.5,.5);
-    this.scale.x = 1;
+    this.scale.x = 2;
+    this.scale.y = 2;
     
     // Physical Properties
     game.physics.enable(this);
     this.body.gravity.y = 1000;
     this.body.collideWorldBounds = true;
-    this.body.setSize(31,48,11,0); // it depends on the child art
-    this.jumpAble = true; 
+    this.body.setSize(18,22,3,9); // it depends on the child art
+  //  this.jumpAble = true; 
 
     // Movement Animations (need art)
-    this.animations.add('move', [0,6,1,7,12,13,3,9], 12, true);
-    this.animations.add('stand', [30,31,32,33,34,35,36,37,38,39], 12, true);
+    this.animations.add('move', [0,1,2], 12, true);
+    this.animations.add('stand', [1], 12, true);
    // this.animations.add('jumping', [37], 12, true);
    // this.animations.add('crouching', [27,28,29,30], 12, true);
 
@@ -30,7 +31,7 @@ Child.prototype.constructor = Child;
 Child.prototype.update = function () {
     // Reset Child velocity
     this.body.velocity.x = 0;
-    this.body.setSize(31,39,11,10);
+   // this.body.setSize(31,39,11,10);
     this.movementManager();
 }
 
@@ -42,18 +43,18 @@ Child.prototype.movementManager = function() {
        // this.body.acceleration.x = Phaser.Math.linearInterpolation([this.body.acceleration.x, 0], 0.01);
         this.animations.play('stand');
     } else if (game.input.keyboard.isDown(Phaser.KeyCode.A)) { // [A] key is down
-        this.scale.x = -1; // face left
+        this.scale.x = -2; // face left
         if (this.body.acceleration.x > -10000) this.body.acceleration.x -= 500; // accelerate
         this.animations.play('move'); 
     } else if (game.input.keyboard.isDown(Phaser.KeyCode.D)) { // [D] key is down
-        this.scale.x = 1; // face right
+        this.scale.x = 2; // face right
         if (this.body.acceleration.x < 10000) this.body.acceleration.x += 500; // accelerate
         this.animations.play('move');
     } else { // Otherwise standing
        this.body.acceleration.x = Phaser.Math.linearInterpolation([this.body.acceleration.x, 0], 0.1);
         // if (this.body.acceleration.x > 0) this.body.acceleration.x -= 1000; // decelerate left
         // else if (this.body.acceleration.x < 0) this.body.acceleration.x += 1000; // decelerate right
-        this.animations.play('stand');
+       this.animations.play('stand');
     } 
     if (this.body.blocked.left || this.body.blocked.right) {
         this.body.acceleration.x = 0;
@@ -65,11 +66,15 @@ Child.prototype.movementManager = function() {
 
     // Jumping
     if (game.input.keyboard.isDown(Phaser.KeyCode.W) && this.body.blocked.down) { 
+        this.animations.play('move');
         this.body.velocity.y = -600;
+        
         // this.body.velocity.y = -650; // debugging
     }
     // Jumping animation
     if (this.body.velocity.y != 0) {
-        this.frame = 37;
+       this.animations.play('move');
+        this.frame = 37;    
+      //  console.log("in air");
     }  
 }
